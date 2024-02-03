d2169d2aa4ee0a258d84e9c8341b57e0de81c7ab
Máté Hidvégi <mhidvegi@gmail.com>
2024-02-03 21:45:33 +0100

    Add AttributeInfo DataAsset
    Video 89

--- Source/TheThrillOfTheHunt/Private/AbilitySystem/TOHAbilitySystemComponent.cpp
+++ Source/TheThrillOfTheHunt/Private/AbilitySystem/TOHAbilitySystemComponent.cpp
@@ -7,11 +7,6 @@
 void UTOHAbilitySystemComponent::AbilityActorInfoSet()
 {
 	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UTOHAbilitySystemComponent::EffectApplied);
-
-	const FTOHGameplayTags& GameplayTags = FTOHGameplayTags::Get();
-
-	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Orange,
-		FString::Printf(TEXT("Tag: %s"), *GameplayTags.Attributes_Secondary_Armor.ToString()));
 }
 
 void UTOHAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent,
