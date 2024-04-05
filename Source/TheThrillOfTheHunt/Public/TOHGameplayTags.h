b353081ed0e01e2a42302a00140347f098a2a0c6
Máté Hidvégi <mhidvegi@gmail.com>
2024-04-05 23:20:11 +0200

    Add dodge ability and play dive animation for it

--- Source/TheThrillOfTheHunt/Public/TOHGameplayTags.h
+++ Source/TheThrillOfTheHunt/Public/TOHGameplayTags.h
@@ -40,10 +40,7 @@ public:
 	FGameplayTag InputTag_LMB;
 	FGameplayTag InputTag_RMB;
 	FGameplayTag InputTag_Shift;
-	FGameplayTag InputTag_1;
-	FGameplayTag InputTag_2;
-	FGameplayTag InputTag_3;
-	FGameplayTag InputTag_4;
+	FGameplayTag InputTag_Dodge;
 
 private:
 	static FTOHGameplayTags GameplayTags;
